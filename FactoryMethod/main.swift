75388dbdb68fcf041cdb8f0105b9af634fcfe808
fierlion <fierlion@gmail.com>
2015-12-16 15:37:22 -0800

    Initial Commit

---
+++ FactoryMethod/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FactoryMethod
+//
+//  Created by Ray Foote on 12/16/15.
+//  Copyright © 2015 fierlion. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
